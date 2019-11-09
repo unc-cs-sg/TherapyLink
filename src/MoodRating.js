14d1d7bd0114b68c01ef37dfbae42272dddf9e29
lin-zhou <zhou.lindsay@yahoo.com>
2019-11-09 13:37:24 -0500

    Outline + stylesheet for Mood Rating view

--- src/MoodRating.js
+++ src/MoodRating.js
@@ -1,15 +1,47 @@
 import React, {Fragment} from 'react';
 import {
   Button,
+  View,
+  Text,
+  Slider,
+  Modal,
+  StyleSheet
 } from 'react-native';
 
+import { moodRatingStyles } from './styles/MoodRatingStyles.js';
+
 class MoodRating extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            mood: 1
+        }
+    }
   static navigationOptions = {
     title: 'Mood Rating',
   };
   render() {
     const {navigate} = this.props.navigation;
-    return <Button title="Go home" onPress={() => navigate('MainScreen')} />;
+    const userName = "Temp";
+    const greeting = "Hi, " + { userName } + "! It's nice to see you again!";
+    const feeling = "How are you feeling today?";
+    return (
+      <View>
+        <Button title="Go Home" onPress={() => navigate('MainScreen')} />
+        <View style={moodRatingStyles.container}>
+            <Text>{ greeting }</Text>
+            <Text>{ feeling }</Text>
+            <Slider
+              step={1}
+              minimumValue={1}
+              maximumValue={5}
+              value={this.state.mood}
+              onValueChange={val => this.setState({ mood: val })}
+            />
+        </View>
+      </View>
+    );
+
   }
 }
 
---
+++ src/styles/MoodRatingStyles.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const colors = {
+  mRBlue: '#9FEBFF'
+}
+
+const moodRatingStyles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.mRBlue,
+    width: "100%"
+  }
+});
+
+export { moodRatingStyles };
