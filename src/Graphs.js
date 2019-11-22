d5c706651c1d4cf44e22576aba43f55f8955995c
Shara He <shara803@gmail.com>
2019-11-21 23:14:20 -0500

    Add Graphing

--- src/Graphs.js
+++ src/Graphs.js
@@ -1,15 +1,69 @@
 import React, {Fragment} from 'react';
+
 import {
   Button,
+  Dimensions,
+  View,
+  Text,
 } from 'react-native';
+
+import {
+  LineChart
+} from 'react-native-chart-kit';
+
 
 class Graphs extends React.Component {
   static navigationOptions = {
     title: 'Graphs',
   };
+
   render() {
-    const {navigate} = this.props.navigation;
-    return <Button title="Go home" onPress={() => navigate('MainScreen')} />;
+
+    return(
+
+        <View>
+          <Button title="Go home" onPress={() => navigate('MainScreen')} />
+          <Text>Progress Chart</Text>
+          <LineChart
+            data={{
+              labels: ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"],
+              datasets: [
+                {
+                  data: [
+                    Math.random() * 5,
+                    Math.random() * 5,
+                    Math.random() * 5,
+                    Math.random() * 5,
+                    Math.random() * 5,
+                    Math.random() * 5,
+                    Math.random() * 5
+                  ]
+                }
+              ]
+            }}
+            width={Dimensions.get("window").width} // from react-native
+            height={220}
+
+            chartConfig={{
+              backgroundColor: "#50B9FF",
+              backgroundGradientFrom: "#50B9FF",
+              backgroundGradientTo: "#B0E6FF",
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              style: {
+                borderRadius: 32
+              },
+              propsForDots: {
+                r: "5",
+                strokeWidth: "1",
+                stroke: "#5698F5"
+              }
+            }}
+
+          />
+        </View>
+    );
   }
 }
 
