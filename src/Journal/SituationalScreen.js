2922788002a2192be9ebc255e6f66a3b9393bbe2
yangIon <yyangc88@gmail.com>
2020-01-01 22:51:12 -0500

    Emotion situational panels

---
+++ src/Journal/SituationalScreen.js
@@ -0,0 +1,38 @@
+import React, { Fragment, Component } from 'react';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    TextInput,
+} from 'react-native';
+import Icon from 'react-native-ionicons';
+
+class SituationalScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: '',
+        };
+    }
+
+    static navigationOptions = {
+        title: '',
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={{ alignItems: 'center', }}>
+                <Text style={{ marginHorizontal: 20, fontWeight: 'bold', fontSize: 20, }}>
+                    What do you think can be done to make the situation better?
+                </Text>
+                <TextInput style={{ textAlignVertical: 'top', width: '80%' }} placeholder="Write out your thoughts..." multiline={true} numberOfLines={10} onChangeText={text => this.setState({ text })} value={this.state.text} />
+                <TouchableOpacity onPress={() => navigate('JournalSummary', { isYes: true })}>
+                    <Icon name="arrow-forward" />
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+export default SituationalScreen;
